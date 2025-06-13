b6f44ad66fa7b00b6ec14f3691f95d22be01e9d7
Umair <umairshahidnl@gmail.com>
2025-06-14 01:58:41 +0200

    feat: integrate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
+  const port = process.env.PORT ?? 3000;
 
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API description')
+    .setVersion('1.0')
+    .addServer(`http://localhost:${port}/`, 'Local environment')
+    .addTag('Your API Tag')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   // Set global prefix for all routes
   app.setGlobalPrefix('api');
 
   // Enable CORS for frontend integration
   app.enableCors();
 
-  const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
   logger.log(`API is running on port: ${port}`);
